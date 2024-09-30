89260d86b2199384eacb92ceb225189d0bfb6daa
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-09-30 11:57:45 +0200

    chore: Apply `const` to most things

--- src/vstdlib/random.cpp
+++ src/vstdlib/random.cpp
@@ -2,35 +2,36 @@
 #include <random>
 #include <iostream>
 
+
 CUniformRandomStream g_UniformRandomStream{};
 IUniformRandomStream* g_pUniformRandomStream{ &g_UniformRandomStream };
 CGaussianRandomStream g_GaussianRandomStream{ &g_UniformRandomStream };
 
 // --- CUniformRandomStream ---
 CUniformRandomStream::CUniformRandomStream() = default;
-void CUniformRandomStream::SetSeed( int iSeed ) {
+void CUniformRandomStream::SetSeed( const int iSeed ) {
 	this->m_Mutex.Lock();
 	this->m_Engine.seed( iSeed );
 	this->m_Mutex.Unlock();
 }
 
-float CUniformRandomStream::RandomFloat( float flMinVal, float flMaxVal ) {
+float CUniformRandomStream::RandomFloat( const float flMinVal, const float flMaxVal ) {
 	this->m_Mutex.Lock();
-	auto res{ std::uniform_real_distribution{ flMinVal, flMaxVal }( this->m_Engine ) };
+	const auto res{ std::uniform_real_distribution{ flMinVal, flMaxVal }( this->m_Engine ) };
 	this->m_Mutex.Unlock();
 	return res;
 }
-int CUniformRandomStream::RandomInt( int iMinVal, int iMaxVal ) {
+int CUniformRandomStream::RandomInt( const int iMinVal, const int iMaxVal ) {
 	this->m_Mutex.Lock();
-	auto res{ std::uniform_int_distribution{ iMinVal, iMaxVal }( this->m_Engine ) };
+	const auto res{ std::uniform_int_distribution{ iMinVal, iMaxVal }( this->m_Engine ) };
 	this->m_Mutex.Unlock();
 	return res;
 }
-float CUniformRandomStream::RandomFloatExp( float flMinVal, float flMaxVal, float flExponent ) {
+float CUniformRandomStream::RandomFloatExp( const float flMinVal, const float flMaxVal, const float flExponent ) {
 	this->m_Mutex.Lock();
-	auto res{ std::uniform_real_distribution{ flMinVal, flMaxVal }( this->m_Engine ) };
+	const auto res{ std::uniform_real_distribution{ flMinVal, flMaxVal }( this->m_Engine ) };
 	this->m_Mutex.Unlock();
-	return pow( res, flExponent );
+	return std::pow( res, flExponent );
 }
 
 // --- CGaussianRandomStream ---
@@ -40,30 +41,31 @@ CGaussianRandomStream::CGaussianRandomStream( IUniformRandomStream* pUniformStre
 void CGaussianRandomStream::AttachToStream( IUniformRandomStream* pUniformStream ) {
 	this->m_pUniformStream = pUniformStream ? pUniformStream : g_pUniformRandomStream;
 }
-float CGaussianRandomStream::RandomFloat( float flMean, float flStdDev ) {
+float CGaussianRandomStream::RandomFloat( const float flMean, const float flStdDev ) {
 	return std::normal_distribution{ flMean, flStdDev }( dynamic_cast<CUniformRandomStream*>( this->m_pUniformStream )->m_Engine );
 }
 
 // --- globals ---
-void RandomSeed( int iSeed ) {
+void RandomSeed( const int iSeed ) {
 	g_pUniformRandomStream->SetSeed( iSeed );
 }
-float RandomFloat( float flMinVal, float flMaxVal ) {
+float RandomFloat( const float flMinVal, const float flMaxVal ) {
 	return g_pUniformRandomStream->RandomFloat( flMinVal, flMaxVal );
 }
-float RandomFloatExp( float flMinVal, float flMaxVal, float flExponent ) {
+float RandomFloatExp( const float flMinVal, const float flMaxVal, const float flExponent ) {
 	return g_pUniformRandomStream->RandomFloatExp( flMinVal, flMaxVal, flExponent );
 }
-int RandomInt( int iMinVal, int iMaxVal ) {
+int RandomInt( const int iMinVal, const int iMaxVal ) {
 	return g_pUniformRandomStream->RandomInt( iMinVal, iMaxVal );
 }
-float RandomGaussianFloat( float flMean, float flStdDev ) {
+float RandomGaussianFloat( const float flMean, const float flStdDev ) {
 	return g_GaussianRandomStream.RandomFloat( flMean, flStdDev );
 }
 
 void InstallUniformRandomStream( IUniformRandomStream* pStream ) {
-	if (! pStream )
+	if ( not pStream ) {
 		pStream = &g_UniformRandomStream;
+	}
 	g_pUniformRandomStream = pStream;
 	g_GaussianRandomStream.AttachToStream( pStream );
 }
