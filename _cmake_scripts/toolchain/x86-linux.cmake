2e8937469dd296e3eaf1cfa6ff7a283826006b40
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-12-02 23:31:29 +0100

    fix: cmake compiling for x86_64 when on linux with gcc

--- _cmake_scripts/toolchain/x86-linux.cmake
+++ _cmake_scripts/toolchain/x86-linux.cmake
@@ -1,11 +1,13 @@
 set( CMAKE_SYSTEM_NAME "Linux" )
-set( CMAKE_SYSTEM_PROCESSOR "x86" )
+set( CMAKE_SYSTEM_PROCESSOR "i386" )
 
-set( CMAKE_C_COMPILER_TARGET "x86-linux" )
-set( CMAKE_CXX_COMPILER_TARGET "x86-linux" )
+set( CMAKE_C_COMPILER_TARGET "i386-linux" )
+set( CMAKE_CXX_COMPILER_TARGET "i386-linux" )
 
 # gcc needs special treatment to target x86
-if ( CMAKE_C_COMPILER STREQUAL "gcc" OR CMAKE_CXX_COMPILER STREQUAL "g++" )
-	set( CMAKE_C_COMPILER gcc -m32 )
-	set( CMAKE_CXX_COMPILER g++ -m32 )
+string( FIND "${CMAKE_C_COMPILER}" "gcc" GCC_POS )
+string( FIND "${CMAKE_CXX_COMPILER}" "g++" GXX_POS )
+if ( NOT (GCC_POS EQUAL -1 AND GXX_POS EQUAL -1) )
+	set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32" )
+	set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" )
 endif ()
